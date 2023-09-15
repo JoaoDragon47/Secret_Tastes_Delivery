d8b25ceeec55128c6145e89a8343d07cd15627cd
Luska <joaolucasmolinda@gmail.com>
2023-09-15 10:02:54 -0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,12 @@
+// function selecionar(id) {
+//     let botaoSelecionado = document.getElementById(id)
+//     botaoSelecionado.style.backgroundColor = "#24a544";
+//     if (botaoBack) {
+//         botaoBack.style.backgroundColor = "#2fcc56"
+//         var botaoBack = document.getElementById(id)
+//     } else {
+//         var botaoBack = document.getElementById(id)
+//     }
+// }
+
+// APERTAR O BOTÃO DE AVALIAÇÃO E DEIXAR O FUNDO COM OUTRA COR; APÓS APERTAR OUTRO A COR DO ANTERIOR VOLTA AO NORMAL
